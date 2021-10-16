ba85fd88a8539d1992795daf832226a462df5f67
Paul Chastain <paulchast1@gmail.com>
2021-10-17 01:22:00 +0200

    random alternativ & mer

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,36 +1,52 @@
 ﻿using System.IO;
 using System;
-using System.Collections.Generic;
 
 
 namespace Quiz
 {
     class Program
     {
-        static void Main()
+        static void Main() {
+            Console.WriteLine("Welcome to this completely random quiz!");
+
+            Console.ReadLine();
+            frågor();
+        }
+
+
+
+        static void frågor()
         {
             string[] fileRead = File.ReadAllLines(@"..\QnA.txt");
-            string[][] frågor = new string[fileRead.Length][];
+            string[][] questions = new string[fileRead.Length][];
 
             int score = 0;
             string answerInput = "";
 
             for (var i = 0; i < fileRead.Length; i++)
             {
-                frågor[i] = fileRead[i].Split(";");
-                Console.WriteLine(frågor[i][0]);
+                Random rdm = new Random();
+                int a = rdm.Next(1, 4);
+                int b = rdm.Next(1, 4);
+                int c = rdm.Next(1, 4);
+
+                questions[i] = fileRead[i].Split(";");
+                Console.WriteLine(questions[i][0]);
+                Console.WriteLine($"a) {questions[i][a]},     b) {questions[i][b]},     c) {questions[i][c]}");
                 answerInput = Console.ReadLine();
 
-                if (answerInput == frågor[i][1])
+                if (answerInput == questions[i][1])
                 {
                     score++;
                 }
+
+                Console.WriteLine();
+                Console.WriteLine($"Your score is {score}.");
+                Console.WriteLine();
+                Console.WriteLine();
             }
 
             Console.ReadLine();
         }
     }
 }
-
-
-// Console.WriteLine(fileRead[0].Split(";")[0]);
