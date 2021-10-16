f2c372c95b5b6e41971535323f42b70d8e121e2d
Paul Chastain <paulchast1@gmail.com>
2021-10-17 00:48:02 +0200

    first commit

---
+++ Quiz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] fileRead = File.ReadAllLines(@"..\QnA.txt");
+            string[][] frågor = new string[fileRead.Length][];
+
+            int score = 0;
+            string answerInput = "";
+
+            for (var i = 0; i < fileRead.Length; i++)
+            {
+                frågor[i] = fileRead[i].Split(";");
+                Console.WriteLine(frågor[i][0]);
+                answerInput = Console.ReadLine();
+
+                if (answerInput == frågor[i][1])
+                {
+                    score++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// Console.WriteLine(fileRead[0].Split(";")[0]);
