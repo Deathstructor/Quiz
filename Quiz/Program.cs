4dd2fb5b3ccef31bd22141237435d1fd5399e1f2
Paul Chastain <paulchast1@gmail.com>
2021-10-17 18:17:02 +0200

    randomized questions

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System;
 
 
@@ -7,22 +8,33 @@ namespace Quiz
     class Program
     {
         static void Main() {
-            Console.WriteLine("Welcome to this completely random quiz!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
 
+            Console.WriteLine("Hello and welcome to this completely random quiz!");
+            Console.WriteLine();
+            Console.WriteLine("You will be given three alternatives for each question.");
+            Console.WriteLine("To choose your answer you need to typ in the letter next");
+            Console.WriteLine("to the alternative (A, B or C).");
+            Console.WriteLine();
+            Console.WriteLine("Good luck! :)");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Press 'Enter' to continue.");
             Console.ReadLine();
-            frågor();
-        }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Clear();
 
 
-
-        static void frågor()
-        {
+        
             string[] fileRead = File.ReadAllLines(@"..\QnA.txt");
             string[][] questions = new string[fileRead.Length][];
 
             int score = 0;
             string answerInput = "";
 
+            List<int> randomQuestion = new List<int>();
             for (var i = 0; i < fileRead.Length; i++)
             {
                 Random rdm = new Random();
@@ -30,9 +42,21 @@ namespace Quiz
                 int b = rdm.Next(1, 4);
                 int c = rdm.Next(1, 4);
 
+                Retry:
+                    int next = rdm.Next(15);
+                    i = next;
+
+                    if (randomQuestion.Contains(next))
+                    {
+                        goto Retry;
+                    } else
+                    {
+                        randomQuestion.Add(next);
+                    }
+
                 questions[i] = fileRead[i].Split(";");
                 Console.WriteLine(questions[i][0]);
-                Console.WriteLine($"a) {questions[i][a]},     b) {questions[i][b]},     c) {questions[i][c]}");
+                Console.WriteLine($"a) {questions[i][a]}     b) {questions[i][b]}     c) {questions[i][c]}");
                 answerInput = Console.ReadLine();
 
                 if (answerInput == questions[i][1])
@@ -44,8 +68,13 @@ namespace Quiz
                 Console.WriteLine($"Your score is {score}.");
                 Console.WriteLine();
                 Console.WriteLine();
-            }
 
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("Press 'Enter' to continue.");
+                Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Clear();
+            }
             Console.ReadLine();
         }
     }
